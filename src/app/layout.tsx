cfbd9a64d2aad173b1f0d62b077d9922bda7fc60
helloiambguedes <bguedesdev@gmail.com>
2024-03-27 14:12:05 +0100

    feat: added gtag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,20 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="pt" data-theme="valentine">
+			<Head>
+				<script async src="https://www.googletagmanager.com/gtag/js?id=G-FKDE4G4396"></script>
+				<script>
+					{`
+						window.dataLayer = window.dataLayer || [];
+						function gtag(){
+							dataLayer.push(arguments)
+						}
+						gtag('js', new Date());
+
+						gtag('config', 'G-FKDE4G4396');
+					`}
+				</script>
+			</Head>
 			<meta name="viewport" content="width=device-width, initial-scale=1" />
 			<body className={inter.className}>{children}</body>
 		</html>
