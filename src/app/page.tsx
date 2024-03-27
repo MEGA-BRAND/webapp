60c9cb51c19cf85f1afc1108f909c6126e450b85
helloiambguedes <bguedesdev@gmail.com>
2024-03-27 14:46:10 +0100

    fix: added error scenario

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 import { HeroSection } from "@/components/hero-section";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function Home() {
 	return (
-		<main className="">
-			<HeroSection />
-		</main>
+		<>
+			<main>
+				<HeroSection />
+			</main>
+			<ToastContainer />
+		</>
 	);
 }
